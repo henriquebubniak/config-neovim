74f812e8332061d02b87a4c04ce02a7cb57affc8
Henrique Bubniak <hg.bubniak@gmail.com>
2024-03-12 17:48:57 -0300

    hybrid relative lines

--- lua/buba/remap.lua
+++ lua/buba/remap.lua
@@ -1,7 +1,9 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.scrolloff = 999
