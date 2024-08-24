3177fe9ede6e4135bfe34e1ca53c1c99d5eacb70
Henrique Bubniak <hg.bubniak@gmail.com>
2024-08-24 09:42:40 -0300

    move cursor between buffers

--- lua/buba/keymaps.lua
+++ lua/buba/keymaps.lua
@@ -9,6 +9,8 @@ vim.g.mapleader = " "
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<S-l>", ":bnext<cr>", opts)
 keymap("n", "<S-h>", ":bprevious<cr>", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("v", ">", ">gv", opts)
 keymap("v", "<", "<gv", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
